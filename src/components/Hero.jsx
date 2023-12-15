31fbbc8568f83750bd3181e145fb3074a58ebb21
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-12-15 07:17:11 +0530

    Build: Hero styles (SCSS)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { HiLocationMarker } from "react-icons/hi";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero-wrapper">
+      <div className="hero-container">
+        <aside className="hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover <br /> Most Suitable <br /> Property
+            </h1>
+          </div>
+          <div className="hero-des">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+          <div className="search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
+        </aside>
+        <aside className="hero-right">
+          <div className="image-container">
+            <img src="./hero-img.png" alt="" />
+          </div>
+        </aside>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
