a54e5a4c88f36319743e06224ba261d5d875d307
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-12-17 09:24:49 +0530

    Feature: Made landing page responsive  for all screens
    Libraries: react-outside-click-handler (https://www.npmjs.com/package/react-outside-click-handler)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
+  const [menuOpend, setMenuOpend] = useState(false);
+  const getMenuStyles = (menuOpend) => {
+    if (document.documentElement.clientWidth < 768) {
+      return {
+        right: !menuOpend && "-100%",
+      };
+    }
+  };
   return (
     <section className="h-wrapper">
       <div className="h-container">
         <img src="./logo.png" alt="Main logo" width={100} />
-        <div className="h-menu">
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
-          <a href="">Contact Us</a>
-          <a href="">Get Started</a>
-          <button className="button">
-            <a href="">Contact</a>
-          </button>
+        <OutsideClickHandler onOutsideClick={() => setMenuOpend(false)}>
+          <div className="h-menu" style={getMenuStyles(menuOpend)}>
+            <a href="">Residencies</a>
+            <a href="">Our Value</a>
+            <a href="">Contact Us</a>
+            <a href="">Get Started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpend((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
         </div>
       </div>
     </section>
