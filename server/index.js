e575a115e781388af95c0c5dfa9401b61195dd71
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-12-21 13:47:06 +0530

    Build: Simple express server to start creating the backend

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
